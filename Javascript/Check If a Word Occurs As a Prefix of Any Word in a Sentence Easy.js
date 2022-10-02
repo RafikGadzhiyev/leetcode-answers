aadc85e3dc23ffe9426d744217480c080f7e43ed
Rafik <66208417+RafikGadzhiyev@users.noreply.github.com>
2022-10-02 23:31:04 +0600

    Create Check If a Word Occurs As a Prefix of Any Word in a Sentence Easy.js

---
+++ Javascript/Check If a Word Occurs As a Prefix of Any Word in a Sentence Easy.js
@@ -0,0 +1,17 @@
+/*
+  Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
+
+Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. 
+If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+
+A prefix of a string s is any leading contiguous substring of s.
+*/
+
+var isPrefixOfWord = function(sentence, searchWord) {
+    sentence = sentence.split(' ');
+    for(let i = 0; i < sentence.length; ++i){
+        if(sentence[i].slice(0, searchWord.length) === searchWord) return i + 1;
+    }
+    
+    return -1;
+};
