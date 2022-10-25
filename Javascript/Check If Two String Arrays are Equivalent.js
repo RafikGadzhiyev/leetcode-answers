d9c4cb1826852adc827304001e01f9ef6ec61383
Rafik <66208417+RafikGadzhiyev@users.noreply.github.com>
2022-10-25 21:33:04 +0600

    Create Check If Two String Arrays are Equivalent.js

---
+++ Javascript/Check If Two String Arrays are Equivalent.js
@@ -0,0 +1,11 @@
+/*
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated in order forms the string.
+
+ 
+*/
+
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.join`` === word2.join``
+};
