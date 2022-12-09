2c02eed2c0c0acaf7434af8a632814810424200a
Rafik <66208417+RafikGadzhiyev@users.noreply.github.com>
2022-12-09 17:48:59 +0600

    Create Single Number.js

---
+++ Single Number.js
@@ -0,0 +1,12 @@
+/*
+  Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+*/
+
+var singleNumber = function(nums) {
+    let result = 0;
+    for(let i = 0; i < nums.length; i++) result ^= nums[i];
+
+    return result;
+};
